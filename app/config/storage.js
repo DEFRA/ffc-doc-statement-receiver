85d24c6bf03fd73afcba57e37343c297a76205ca
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-07-22 11:42:11 +0100

    removed comments left over

--- app/config/storage.js
+++ app/config/storage.js
@@ -1,6 +1,5 @@
 const Joi = require('joi')
 
-// Define config schema
 const schema = Joi.object({
   connectionStr: Joi.string().when('useConnectionStr', { is: true, then: Joi.required(), otherwise: Joi.allow('').optional() }),
   storageAccount: Joi.string().required(),
@@ -10,7 +9,6 @@ const schema = Joi.object({
   createContainers: Joi.boolean().default(true)
 })
 
-// Build config
 const config = {
   connectionStr: process.env.AZURE_STORAGE_CONNECTION_STRING,
   storageAccount: process.env.AZURE_STORAGE_ACCOUNT_NAME,
@@ -20,12 +18,10 @@ const config = {
   createContainers: process.env.AZURE_STORAGE_CREATE_CONTAINERS
 }
 
-// Validate config
 const result = schema.validate(config, {
   abortEarly: false
 })
 
-// Throw if config is invalid
 if (result.error) {
   throw new Error(`The blob storage config is invalid. ${result.error.message}`)
 }
