feb41d75b1aec20878ba773cc12b650d864db36d
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-07-25 10:10:19 +0100

    replace redis port to standard 6379

--- app/insights.js
+++ app/insights.js
@@ -4,7 +4,6 @@ const setup = () => {
   if (process.env.APPINSIGHTS_CONNECTIONSTRING) {
     appInsights.setup(process.env.APPINSIGHTS_CONNECTIONSTRING).start()
     console.log('App Insights running')
-
     if (appInsights.defaultClient) {
       const cloudRoleTag = appInsights.defaultClient.context.keys.cloudRole
       const appName = process.env.APPINSIGHTS_CLOUDROLE
