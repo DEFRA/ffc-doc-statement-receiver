c55cfbcc5488fca8583cc657a74f53404dea1bf6
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-07-29 16:29:50 +0100

    tweaked integration tests

--- app/server.js
+++ app/server.js
@@ -4,30 +4,36 @@ const Hapi = require('@hapi/hapi')
 const config = require('./config')
 
 const createServer = async () => {
-  const server = Hapi.server({
-    port: config.port,
-    cache: [{
-      name: config.cache.cacheName,
-      provider: {
-        constructor: config.cache.catbox,
-        options: config.cache.catboxOptions
-      }
-    }]
-  })
+  try {
+    const server = Hapi.server({
+      port: config.port,
+      cache: [{
+        name: config.cache.cacheName,
+        provider: {
+          constructor: config.cache.catbox,
+          options: config.cache.catboxOptions
+        }
+      }]
+    })
 
-  const cache = server.cache({ cache: config.cache.cacheName, segment: config.cache.segment, expiresIn: config.cache.ttl })
-  server.app.cache = cache
+    const cache = server.cache({ cache: config.cache.cacheName, segment: config.cache.segment, expiresIn: config.cache.ttl })
+    server.app.cache = cache
 
-  await server.register(require('./plugins/enabled'))
-  await server.register(require('./plugins/errors'))
-  await server.register(require('./plugins/router'))
-  await server.register(require('./plugins/logging'))
+    await server.register(require('./plugins/enabled'))
+    await server.register(require('./plugins/errors'))
+    await server.register(require('./plugins/router'))
+    await server.register(require('./plugins/logging'))
 
-  if (config.isDev) {
-    await server.register(require('blipp'))
+    if (config.isDev) {
+      await server.register(require('blipp'))
+    }
+
+    console.log('Server created successfully')
+    return server
+  } catch (error) {
+    console.error('Error creating server:', error)
+    throw error
   }
-
-  return server
 }
 
 module.exports = createServer
