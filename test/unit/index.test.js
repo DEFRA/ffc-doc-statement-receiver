c077f89ecba11750f4c6b95ab641f92f0969244f
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-07-29 16:29:50 +0100

    tweaked integration tests

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,6 +1,4 @@
-const { init } = require('../../app/index')
 const createServer = require('../../app/server')
-
 jest.mock('../../app/server')
 
 describe('Server initialization', () => {
@@ -16,19 +14,23 @@ describe('Server initialization', () => {
     const mockServer = { start: mockStart, info: { uri: 'http://localhost:3022' } }
     createServer.mockResolvedValue(mockServer)
 
-    await init()
+    jest.isolateModules(() => {
+      require('../../app/index')
+    })
 
+    await new Promise(process.nextTick)
     expect(mockStart).toHaveBeenCalled()
     expect(logSpy).toHaveBeenCalledWith('Server running on %s', 'http://localhost:3022')
   })
 
   test('handles unhandledRejection by logging the error and exiting with status 1', async () => {
+    require('../../app/index')
+
     process.emit('unhandledRejection', new Error('Test Error'))
 
     await new Promise(process.nextTick)
 
     expect(logSpy).toHaveBeenCalledWith(expect.any(Error))
-
     expect(exitSpy).toHaveBeenCalledWith(1)
   })
 })
