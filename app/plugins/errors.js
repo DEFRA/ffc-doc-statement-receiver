4a77af0544d7d101307d1356b927457091a4e52c
John Barnard <john.barnard.external@eviden.com>
2026-04-21 13:16:44 +0100

    updated hapi and boom and corrected usage
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/plugins/errors.js
+++ app/plugins/errors.js
@@ -11,7 +11,7 @@ module.exports = {
           request.log('error', {
             statusCode,
             message: response.message,
-            payloadMessage: response.data ? response.data.payload.message : ''
+            payloadMessage: response.data?.defaultError?.output?.payload?.message ?? ''
           })
           return response
         }
